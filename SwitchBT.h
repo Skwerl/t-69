142371780af314033137a812b3287a8f673dc382
Kevin Cogill <skwerl@gmail.com>
2022-08-08 06:40:09 -0700

    Initial check-in of Switch-controlled L/R head movement

---
+++ SwitchBT.h
@@ -0,0 +1,56 @@
+#ifndef _switchbt_h_
+#define _switchbt_h_
+
+#include "SwHID.h"
+
+/**
+   This class implements support for the PS4 controller via Bluetooth.
+   It uses the BTHID class for all the Bluetooth communication.
+*/
+class SwitchBT : public SwHID {
+  
+  public:
+    
+    SwitchBT(BTD *p, bool pair = false, const char *pin = "0000") :
+      SwHID(p, pair, pin) {
+    };
+    
+    bool connected() {
+      return SwHID::connected;
+    };
+
+    byte Report[3]; 
+
+  protected:
+
+    virtual void ParseSwHIDData(uint8_t len, uint8_t *buf) {
+
+      Report[0] = buf[0];
+      Report[1] = buf[1];
+      Report[2] = buf[2];
+
+      /*
+      Serial.print(buf[0]); Serial.print(" ");
+      Serial.print(buf[1]); Serial.print(" ");
+      Serial.print(buf[2]); Serial.print(" ");
+      Serial.println();
+      */
+      
+    };
+
+    virtual void OnInitSwHID() {
+      return;
+    };
+
+    virtual void ResetSwHID() {
+      return;
+    };
+
+  private:
+    
+    void HID_Command(uint8_t *data, uint8_t nbytes) {
+      pBtd->L2CAP_Command(hci_handle, data, nbytes, control_scid[0], control_scid[1]);
+    };
+
+};
+#endif
---
+++ t-69.ino
@@ -0,0 +1,331 @@
+/*////////////////////////////////////////////////////////////////////////////////////////////////*/
+///////////////////////* Libraries *////////////////////////////////////////////////////////////////
+/*////////////////////////////////////////////////////////////////////////////////////////////////*/
+
+#include <Wire.h>
+#include <Servo.h>
+#include <SoftwareSerial.h>
+#include <Adafruit_PWMServoDriver.h>
+
+#include "SwitchBT.h"
+#include <usbhub.h>
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////*/
+///////////////////////* Bluetooth Config */////////////////////////////////////////////////////////
+/*////////////////////////////////////////////////////////////////////////////////////////////////*/
+
+USB Usb;
+BTD Btd(&Usb);
+
+// To Pair:
+SwitchBT Switch(&Btd, PAIR);
+
+// After Pair:
+//SwitchBT Switch(&Btd);
+
+bool bluetoothInit = false;
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////*/
+///////////////////////* Servo Config */////////////////////////////////////////////////////////////
+/*////////////////////////////////////////////////////////////////////////////////////////////////*/
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+#define SERVO_FREQ 50
+#define SERVO_OSC_FREQ 27000000
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////*/
+///////////////////////* Head Left/Right *//////////////////////////////////////////////////////////
+/*////////////////////////////////////////////////////////////////////////////////////////////////*/
+
+uint8_t servonum = 2;
+
+float xmin = 200;
+float xmax = 460;
+float xcenter = xmin + ((xmax - xmin) / 2);
+float xtarget = xcenter;
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////*/
+///////////////////////* Switch Parsing *///////////////////////////////////////////////////////////
+/*////////////////////////////////////////////////////////////////////////////////////////////////*/
+
+byte* Report;
+
+struct SwitchButtons {
+  bool Idle;
+  bool U_Button;
+  bool D_Button;
+  bool L_Button;
+  bool R_Button;
+  bool Y_Button;
+  bool X_Button;
+  bool B_Button;
+  bool A_Button;
+  bool SL_Button;
+  bool SR_Button;
+  bool L_Trigger;
+  bool ZL_Trigger;
+  bool R_Trigger;
+  bool ZR_Trigger;
+  bool Stick_Button;
+  bool Select_Button;
+  bool Action_Button;
+  int Analog_Stick;
+};
+SwitchButtons reset = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+SwitchButtons state = reset;
+
+void handleEvent() {
+
+  if (Report[0] == 0 && Report[1] == 0 && Report[2] == 8) {
+
+    allButtonsReleased();
+    state.Idle = 1;
+
+  } else {
+
+    state.Idle = 0;
+
+    switch (Report[0]) {
+      case 4:
+        state.U_Button = 1;
+        state.B_Button = 1;
+        break;
+      case 2:
+        state.D_Button = 1;
+        state.X_Button = 1;
+        break;
+      case 1:
+        state.L_Button = 1;
+        state.A_Button = 1;
+        break;
+      case 8:
+        state.R_Button = 1;
+        state.Y_Button = 1;
+        break;
+      case 16:
+        state.SL_Button = 1;
+        break;
+      case 32:
+        state.SR_Button = 1;
+        break;
+    }
+    switch (Report[1]) {
+      case 64:
+        state.L_Trigger = 1;
+        state.R_Trigger = 1;
+        break;
+      case 128:
+        state.ZL_Trigger = 1;
+        state.ZR_Trigger = 1;
+        break;
+      case 1:
+      case 2:
+        state.Select_Button = 1;
+        break;
+      case 32:
+        state.Action_Button = 1;
+        break;
+      case 4:
+      case 8:
+        state.Stick_Button = 1;
+        break;
+    }
+    switch (Report[2]) {
+      case 8:
+        state.Analog_Stick = 0;
+        break;
+      case 7:
+        state.Analog_Stick = 45;
+        break;
+      case 0:
+        state.Analog_Stick = 90;
+        break;
+      case 1:
+        state.Analog_Stick = 135;
+        break;
+      case 2:
+        state.Analog_Stick = 180;
+        break;
+      case 3:
+        state.Analog_Stick = 225;
+        break;
+      case 4:
+        state.Analog_Stick = 270;
+        break;
+      case 5:
+        state.Analog_Stick = 315;
+        break;
+      case 6:
+        state.Analog_Stick = 360;
+        break;
+    }
+
+  }
+
+  if (!bluetoothInit) {
+    if (state.Analog_Stick > 0) {
+      return;
+    } else {
+      bluetoothInit = true;
+      Serial.println("");
+      Serial.println("Bluetooth Connection Established");
+      wakeUp();
+    }
+  } else {
+
+      Serial.print("IDL");
+      Serial.print(state.Idle);
+      Serial.print(".");
+      Serial.print("UBT");
+      Serial.print(state.U_Button);
+      Serial.print(".");
+      Serial.print("DBT");
+      Serial.print(state.D_Button);
+      Serial.print(".");
+      Serial.print("LBT");
+      Serial.print(state.L_Button);
+      Serial.print(".");
+      Serial.print("RBT");
+      Serial.print(state.R_Button);
+      Serial.print(".");
+      Serial.print("SLB");
+      Serial.print(state.SL_Button);
+      Serial.print(".");
+      Serial.print("SRB");
+      Serial.print(state.SR_Button);
+      Serial.print(".");
+      Serial.print("LTR");
+      Serial.print(state.L_Trigger);
+      Serial.print(".");
+      Serial.print("ZLT");
+      Serial.print(state.ZL_Trigger);
+      Serial.print(".");
+      Serial.print("STK");
+      Serial.print(state.Stick_Button);
+      Serial.print(".");
+      Serial.print("SEL");
+      Serial.print(state.Select_Button);
+      Serial.print(".");
+      Serial.print("ACK");
+      Serial.print(state.Action_Button);
+      Serial.print(".");
+      Serial.print("ANA");
+      Serial.print(state.Analog_Stick);
+      Serial.println("");
+
+    doAction();
+
+  }
+
+}
+
+void allButtonsReleased() {
+  state = reset;
+}
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////*/
+///////////////////////* BB-8 Logic *///////////////////////////////////////////////////////////////
+/*////////////////////////////////////////////////////////////////////////////////////////////////*/
+
+#define SERIAL_BAUD 115200
+
+boolean driveN = false;
+boolean driveS = false;
+boolean driveE = false;
+boolean driveW = false;
+
+void doAction(void) {
+
+  driveS = state.U_Button;
+  driveN = state.D_Button;
+  driveE = state.L_Button;
+  driveW = state.R_Button;
+
+  if (state.Analog_Stick > 0) {
+    //if (state.Analog_Stick >= 315 || state.Analog_Stick <= 045) { driveS = true; }
+    if (state.Analog_Stick == 360) {
+      driveW = true;
+    }
+    //if (state.Analog_Stick >= 135 && state.Analog_Stick <= 225) { driveN = true; }
+    if (state.Analog_Stick == 180) {
+      driveE = true;
+    }
+  }
+
+  if (driveW) {
+    xtarget++;
+  }
+  if (driveE) {
+    xtarget--;
+  }
+  if (xtarget <= xmin) {
+    xtarget = xmin;
+  }
+  if (xtarget >= xmax) {
+    xtarget = xmax;
+  }
+
+  if (!driveE && !driveW) {
+    xtarget = xcenter;
+  }
+
+  if (state.Action_Button) {
+    restDrive();
+  }
+
+  sendMove();
+
+}
+
+void restDrive(void) {
+  //Serial.println("restDrive");
+}
+
+void sendMove(void) {
+  //Serial.println(xtarget);
+  pwm.setPWM(servonum, 0, xtarget);
+}
+
+void wakeUp() {
+  Serial.println("wakeUp");
+}
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////*/
+///////////////////////* Arduino Runtime *//////////////////////////////////////////////////////////
+/*////////////////////////////////////////////////////////////////////////////////////////////////*/
+
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+void setup(void) {
+
+  // PWM, Servo
+  pwm.begin();
+  pwm.setOscillatorFrequency(SERVO_OSC_FREQ);
+  pwm.setPWMFreq(SERVO_FREQ);
+
+  // Bluetooth
+  Serial.begin(SERIAL_BAUD);
+  if (Usb.Init() == -1) {
+    Serial.print(F("\r\nOSC did not start"));
+    while (1); // Halt
+  }
+  Serial.print(F("\r\nSwitch Bluetooth Library Started"));
+
+}
+
+void loop(void) {
+
+  Usb.Task();
+
+  if (Switch.connected()) {
+
+    Report = Switch.Report;
+    handleEvent();
+
+  }
+
+}
